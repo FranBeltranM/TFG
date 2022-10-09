45c6898c8781386762be9542d630142fcf11d7aa
Fran Beltran <contacto@franbeltran.es>
2022-10-09 16:44:39 +0200

    ✨ Swagger documentation ✨

--- backend_v3/src/v1/routes/index.ts
+++ backend_v3/src/v1/routes/index.ts
@@ -2,6 +2,23 @@ import express from 'express'
 
 const router = express.Router()
 
+/**
+ * @openapi
+ * /api/v1:
+ *   get:
+ *     summary: Show the a welcome message
+ *     tags:
+ *       - Index
+ *     responses:
+ *       200:
+ *         description: Welcome message
+ *         content:
+ *           text/html:
+ *             schema:
+ *                 type: string
+ *                 example:
+ *                   - <h1>Welcome to my api 😊</h1>
+ */
 router.get('/', (_, res) => res.status(200).send('<h1>Welcome to my api 😊</h1>'))
 
 export default router
--- backend_v3/src/v1/routes/vehicle.ts
+++ backend_v3/src/v1/routes/vehicle.ts
@@ -3,6 +3,89 @@ import * as vehicleController from '../../controllers/vehicleController'
 
 const router = express.Router()
 
+/**
+ * @openapi
+ * /api/v1/vehicles/search:
+ *   get:
+ *     summary: Get vehicle by VIN or plate
+ *     description: Get vehicle by VIN or plate
+ *     tags:
+ *       - Vehicle
+ *     parameters:
+ *       - in: query
+ *         name: vin
+ *         schema:
+ *           type: string
+ *         description: Vehicle identification number
+ *         example: WVWZZZ1JZ3B177177
+ *       - in: query
+ *         name: plate
+ *         schema:
+ *           type: string
+ *         description: Vehicle plate
+ *     responses:
+ *       200:
+ *         description: Get vehicle by VIN or plate
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                    id:
+ *                      type: string
+ *                    vin:
+ *                      type: string
+ *                    brand:
+ *                      type: string
+ *                    model:
+ *                      type: string
+ *                    plateType:
+ *                      type: string
+ *                    fuel:
+ *                      type: string
+ *                    engineSize:
+ *                      type: number
+ *                    fiscalHP:
+ *                      type: number
+ *                    emissions:
+ *                      type: string
+ *       400:
+ *         description: Bad request, missing parameters or invalid parameters
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                  type: string
+ *                 info:
+ *                  type: string
+ *               example:
+ *                 - status: "KO"
+ *                   info: "Missing fields 🙄"
+ *                 - status: "KO"
+ *                   info: "Invalid plate format 🙄"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                  type: string
+ *                 info:
+ *                  type: string
+ *               example:
+ *                 - status: "KO"
+ *                   info: "Internal server error 😱"
+
+ */
 router.get('/search', vehicleController.checkValidInput, vehicleController.getVehicle)
 
 export default router
